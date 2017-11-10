e6ac2d521f1ba6a3365441a3fee715d78d57ca39
Tyler <tgray.it@gmail.com>
2017-11-10 20:53:54 +0000

    Updated 'js/scripts.js' via CloudCannon

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
-  var classes = ['1','2','3'];
-    $('.project-item').each(function(i) {
-        $(this).addClass(
-          classes[Math.floor(Math.random()*classes.length)]);
-    });
-    $('.half-column').each(function(i) {
-        $(this).addClass(
-            classes[Math.floor(Math.random()*classes.length)]);
-    });
-  //Menu
+
   $(".icon").click(function () {
       $(".mobile-nav").fadeToggle(500);
       $(".top-menu").toggleClass("top-animate");
@@ -23,31 +14,7 @@ $(document).ready(function () {
       $(".bottom-menu").toggleClass("bottom-animate");
       $(".menui").toggleClass("iconDark");
   });
-  if (screen.width > 920) {
-    $('.1').addClass("hidden").viewportChecker({
-        classToAdd: 'visible animated slow1 fadeIn',
-        offset: 100
-       });
-    $('.2').addClass("hidden").viewportChecker({
-       classToAdd: 'visible animated slow2 fadeIn',
-       offset: 100
-      });
-    $('.3').addClass("hidden").viewportChecker({
-      classToAdd: 'visible animated slow3 fadeIn',
-      offset: 100
-     });
-    $('.4').addClass("hidden").viewportChecker({
-       classToAdd: 'visible animated slow4 fadeIn',
-       offset: 100
-      });
-    $('.5').addClass("hidden").viewportChecker({
-        classToAdd: 'visible animated slow5 fadeIn',
-        offset: 100
-       });
-    }
-    //$('.projectItemImage').hover(function() {
-    //  $('projectItemImageOverlay').addClass('visible');
-    //});
+  
 
   $(document).on("scroll", onScroll);
 
